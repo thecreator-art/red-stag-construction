a8bec8f124d679650d4b65dba636574dc5b83539
Damian Jonathan Valencia <damian@Damians-MacBook-Pro.local>
2026-03-24 21:07:53 -0700

    Optimize CSS delivery config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ["framer-motion", "leaflet", "react-leaflet"],
+  },
 };
 
 export default nextConfig;
